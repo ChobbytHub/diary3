86eb0098c5647556f74354ed3ff989ed62434af0
chobby <aa.ing85@gmail.com>
2025-04-09 15:34:31 +0900

    first commit

---
+++ src/main/java/com/chobby/diary3/Diary3Application.java
@@ -0,0 +1,13 @@
+package com.chobby.diary3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diary3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Diary3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chobby/diary3/Diary3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chobby.diary3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diary3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
