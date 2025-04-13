afab38df99a1db30b03c3aab5ec4865964dbb87e
chobby <aa.ing85@gmail.com>
2025-04-13 18:50:56 +0900

    change configuration

---
+++ backend/src/test/java/com/chobby/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chobby.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
