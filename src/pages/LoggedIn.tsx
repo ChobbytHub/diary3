f7c1ea76543c7d4531ac8a3da6fe5e0737419409
chobby <aa.ing85@gmail.com>
2025-04-22 20:41:25 +0900

    fix selectedDate bug

--- src/pages/LoggedIn.tsx
+++ src/pages/LoggedIn.tsx
@@ -1,11 +1,6 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import { useGetDiariesQuery } from "../api/diaryApi";
-import { useAppDispatch } from "../redux/hooks";
-import { setDiaries } from "../redux/slices/diarySlice";
-import { setSelectedDate } from "../redux/slices/selectedDateSlice";
-import { toIsoString } from "../utils/dateUtils";
 
 import Button from "../components/ui/Button";
 import Diary from "../features/diary/Diary";
@@ -13,18 +8,6 @@ import Diary from "../features/diary/Diary";
 export default function LoggedIn() {
   const navigate = useNavigate();
   const { logout } = useContext(AuthContext);
-  const dispatch = useAppDispatch();
-
-  // APIから日記データ取得
-  const { data: diaries, isSuccess } = useGetDiariesQuery();
-
-  // 初期表示時にデータをグローバルステートに保存
-  useEffect(() => {
-    if (isSuccess && diaries) {
-      dispatch(setDiaries(diaries)); // 全日記を格納
-      dispatch(setSelectedDate(toIsoString(new Date()))); // 今日の日付をセット
-    }
-  }, [isSuccess, diaries, dispatch]);
 
   return (
     <div>
